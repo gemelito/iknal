492e491143ec2d21d2d52a2426b62bcafb9dd074
gemelito <pedro.canche@uimqroo.edu.mx>
2017-07-19 11:47:08 -0500

    It is firm->pedro misael canche angulo

--- panelalumnos/includes/head.php
+++ panelalumnos/includes/head.php
@@ -2,6 +2,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 <meta name="theme-color" content="#7cb342">
+<meta name='owner' content='Pedro Misael Canche Angulo'>
+<meta  name='author' content='pedro.canche@uimqroo.edu.mx'>
 <link rel="stylesheet" type="text/css" href="../css/materialize.css">
 <link rel="stylesheet" type="text/css" href="../css/icons.css">
 <link rel="stylesheet" type="text/css" href="../css/common.css">
